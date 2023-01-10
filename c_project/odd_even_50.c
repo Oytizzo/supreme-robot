e407685b5330d9e90441931408f719f3e01b1c9c
oytizzo <oytizzo@gmail.com>
2023-01-10 19:01:55 +0600

    Update odd even

--- c_project/odd_even_50.c
+++ c_project/odd_even_50.c
@@ -4,7 +4,17 @@ int main()
 {
     int n, i;
 
-    for(i=1; i <= 5; i++){
+    //for(i=1; i <= 10; i++){
+    //    scanf("%d", &n);
+    //    if(n % 2 == 0){
+    //        printf("even\n");
+    //    }
+    //    else {
+    //        printf("odd\n");
+    //    }
+    //}
+    i = 1;
+    while(i <= 10){
         scanf("%d", &n);
         if(n % 2 == 0){
             printf("even\n");
@@ -12,6 +22,7 @@ int main()
         else {
             printf("odd\n");
         }
+        i++;
     }
 
     return 0;
