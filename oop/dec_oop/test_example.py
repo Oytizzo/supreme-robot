e8313e6ce2ba57d68ccbf3952b757051ba5af372
oytizzo <oytizzo@gmail.com>
2023-03-14 12:15:34 +0600

    Remove redundant comments

--- oop/dec_oop/test_example.py
+++ oop/dec_oop/test_example.py
@@ -7,10 +7,6 @@ import pytest
 def test_something():
     assert 1 == 1
 
-# When you run your pytest tests, you should see log messages that indicate when the function starts
-# and finishes in the specified log file. Note that you'll need to configure the logging system appropriately
-# to see the log messages in your output.
-
 
 @class_logger
 class TestSomething:
