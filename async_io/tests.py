50468872521a71f7557dd96fc107d8ee97c4dedd
oytizzo <oytizzo@gmail.com>
2023-06-16 13:19:50 +0600

    [add] tests asyncio

--- async_io/tests.py
+++ async_io/tests.py
@@ -2,12 +2,15 @@ import asyncio
 
 async def task1():
     # Task 1 implementation
+    pass
 
 async def task2():
     # Task 2 implementation
+    pass
 
 async def task3():
     # Task 3 implementation
+    pass
 
 # Execute tasks asynchronously
 async def main():
